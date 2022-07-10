59d315d1880db73b64c8ae9992a1c195c1007477
Matias <matias.andres.mh@gmail.com>
2022-07-10 16:50:33 -0400

    counter Finalizado

---
+++ src/CounterApp.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import React from 'react';
+
+
+const CounterApp = ({value}) => {
+
+    const [counter,setCounter] = useState(0);
+
+    const  handleAdd = ()=>{
+     
+        setCounter(counter+1);
+
+    }
+    const handleSubtract = () =>{
+        setCounter(counter-1)
+    }
+    const handleReset = ()=>{
+        setCounter(value)
+    }
+  return (
+    <>
+
+    <h1>CounterApp</h1>
+    <h2>{counter}</h2>
+    <button className='btn  btn-primary' onClick={handleAdd}>+1</button>
+    <button className='btn  btn-danger' onClick={handleSubtract}>-1</button>
+    <button className='btn  btn-warning' onClick={handleReset}>Reset</button>
+
+    </>
+    
+  )
+}
+
+export default CounterApp
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
--- src/HelloWordApp.jsx
+++
@@ -1,4 +0,0 @@
-export function App(){
-    return (<h1>Hola Mundo</h1>);
-}
-export const hola = '123';
